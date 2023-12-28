8d47ed92564a272f4dde71e1d82226670574dceb
Baelrin <Baelrin-Revel@yandex.ru>
2023-12-28 03:34:13 +0300

    Improved file handling and error handling in encryption script
    This commit introduces improvements to the file handling and error handling in the encryption script. The changes include:
    
    - Handling of file existence and non-existence checks
    - Separate handling for encryption and decryption exceptions
    - Use of constants for default file names
    - Improved error messages
    
    These changes aim to enhance the robustness and usability of the script.

--- master.py
+++ master.py
@@ -1,9 +1,77 @@
+import os
+import argparse
 import pyAesCrypt
 
-password = input('Enter password: ')
+INPUT_FILE_DEFAULT = 'data.txt'
+OUTPUT_FILE_DEFAULT = 'data.txt.aes'
+DECRYPT_OUTPUT_DEFAULT = 'dataout.txt'
 
-# encrypt
-pyAesCrypt.encryptFile('data.txt', 'data.txt.aes', password)
+def ensure_file_exists(filename):
+   """Ensure a file exists and raise an exception if it does not."""
+   if not os.path.exists(filename):
+       raise FileNotFoundError(f"{filename} does not exist.")
 
-# decrypt
-pyAesCrypt.decryptFile('data.txt.aes', 'dataout.txt', password)
+def ensure_file_does_not_exist(filename):
+   """Ensure a file does not exist and raise an exception if it does."""
+   if os.path.exists(filename):
+       raise FileExistsError(f"{filename} already exists.")
+
+def encrypt_file(input_filename, output_filename, password):
+   """Encrypt a file using a password.
+
+   Args:
+   input_filename: The name of the file to encrypt.
+   output_filename: The name of the file to write the encrypted data to.
+   password: The password to use for encryption.
+
+   Raises:
+   FileNotFoundError: If the input file does not exist.
+   FileExistsError: If the output file already exists.
+   """
+   try:
+       ensure_file_exists(input_filename)
+       ensure_file_does_not_exist(output_filename)
+       pyAesCrypt.encryptFile(input_filename, output_filename, password)
+   except FileNotFoundError as e:
+       print(f"Input file error: {e}")
+   except FileExistsError as e:
+       print(f"Output file error: {e}")
+   except Exception as e:
+       print(f"Unexpected error: {e}")
+
+def decrypt_file(input_filename, output_filename, password):
+   """Decrypt a file using a password.
+
+   Args:
+   input_filename: The name of the file to decrypt.
+   output_filename: The name of the file to write the decrypted data to.
+   password: The password to use for decryption.
+
+   Raises:
+   FileNotFoundError: If the input file does not exist.
+   FileExistsError: If the output file already exists.
+   """
+   try:
+       ensure_file_exists(input_filename)
+       ensure_file_does_not_exist(output_filename)
+       pyAesCrypt.decryptFile(input_filename, output_filename, password)
+   except FileNotFoundError as e:
+       print(f"Input file error: {e}")
+   except FileExistsError as e:
+       print(f"Output file error: {e}")
+   except Exception as e:
+       print(f"Unexpected error: {e}")
+
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--password', required=True, help='The password to use for encryption and decryption')
+   parser.add_argument('--input', default=INPUT_FILE_DEFAULT, help='The input file to encrypt')
+   parser.add_argument('--output', default=OUTPUT_FILE_DEFAULT, help='The output file for encrypted data')
+   parser.add_argument('--decrypt_output', default=DECRYPT_OUTPUT_DEFAULT, help='The output file for decrypted data')
+   args = parser.parse_args()
+
+   # encrypt
+   encrypt_file(args.input, args.output, args.password)
+
+   # decrypt
+   decrypt_file(args.output, args.decrypt_output, args.password)
